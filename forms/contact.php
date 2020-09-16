29de47e3333b1ea373f8b9fc6c5ee3f74e4a3efe
priyesh <priyeshs94@gmail.com>
2020-09-16 20:54:19 +0530

    Changes to logo

--- forms/contact.php
+++ forms/contact.php
@@ -9,19 +9,19 @@
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'nikhil.joseph01@gmail.com';
 
-//  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
- //   include( $php_email_form );
- // } else {
-    //die( 'Unable to load the "PHP Email Form" Library!');
- // }
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
 
- // $contact = new PHP_Email_Form;
-//  $contact->ajax = true;
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
   
-//  $contact->to = $receiving_email_address;
-//  $contact->from_name = $_POST['name'];
-////  $contact->from_email = $_POST['email'];
- // $contact->subject = $_POST['subject'];
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
@@ -33,17 +33,9 @@
   );
   */
 
- // $contact->add_message( $_POST['name'], 'From');
- //// $contact->add_message( $_POST['email'], 'Email');
- // $contact->add_message( $_POST['message'], 'Message', 10);
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
 
-  $to = 'priyeshs94@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-//$headers .= "Reply-To: $email_address";	
-
-  mail($to,$email_subject,$email_body,$headers);
-  return true;
-  //echo $contact->send();
+  echo $contact->send();
 ?>
